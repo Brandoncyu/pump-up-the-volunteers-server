a87bbd45ed49ab60e67dcd2400d65ac303dd0679
Brandon <brandoncyu@gmail.com>
2018-08-06 11:45:31 -0700

    changed tables

--- src/db/migrations/20180803132951_organizations.js
+++ src/db/migrations/20180803132951_organizations.js
@@ -1,15 +1,23 @@
 const table = 'organizations'
 exports.up = knex => {
-    return knex.schema.createTable(table, table => {
-        table.increments()
-        table.string('name').notNullable()
-        table.string('email').notNullable().unique()
-        table.text('password').notNullable()
-        table.integer('ein').notNullable().unique()
-        table.timestamps(true, true)
-    })
+  return knex.schema.createTable(table, table => {
+    table.increments()
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.text('password').notNullable()
+    table.integer('ein').notNullable().unique()
+    table.string('description').notNullable()
+    table.string('logo').notNullable()
+    table.string('street')
+    table.string('city')
+    table.string('zipCode')
+    table.string('latitude')
+    table.string('longitude')
+    table.foreign('option_id').references('options.id').onDelete('CASCADE')
+    table.timestamps(true, true)
+  })
 };
 
-exports.down = function (knex, Promise) {
-    return knex.schema.dropTable(table)
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable(table)
 };
