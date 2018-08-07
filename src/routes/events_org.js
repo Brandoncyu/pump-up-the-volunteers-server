d4d032e2302848cbf8daa31f8b9b70bf103645dd
Brandon <brandoncyu@gmail.com>
2018-08-07 13:24:55 -0700

    fixed tables, added return for await

--- src/routes/events_org.js
+++ src/routes/events_org.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-const ctrl = require('../controllers/events_org')
-const auth = require('../lib/auth')
-
-router.get('/', auth.isLoggedIn, ctrl.index)
-router.getOne('/:id', auth.isLoggedIn, ctrl.getOne)
-router.post('/', auth.isLoggedIn, ctrl.create)
-router.delete('/:id', auth.isAuthorized, ctrl.destroy)
-
-module.exports = router
+// const router = require('express').Router()
+// const ctrl = require('../controllers/events_org')
+// const auth = require('../lib/auth')
+//
+// router.get('/', auth.isLoggedIn, ctrl.index)
+// router.getOne('/:id', auth.isLoggedIn, ctrl.getOne)
+// router.post('/', auth.isLoggedIn, ctrl.create)
+// router.delete('/:id', auth.isAuthorized, ctrl.destroy)
+//
+// module.exports = router
