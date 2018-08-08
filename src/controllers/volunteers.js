d3f12e4fd16228178cf5b233f2c2beda154a261b
Brandon <brandoncyu@gmail.com>
2018-08-08 16:52:41 -0700

    added new days parser function

--- src/controllers/volunteers.js
+++ src/controllers/volunteers.js
@@ -4,9 +4,20 @@ const auth = require('../lib/auth')
 async function signup(req, res, next) {
   try {
     const response = await model.create(req.body)
+    const interestList = await model.interestFinder(response.id)
     const token = auth.createToken(response.id)
+    const days = [
+      response.Sunday,
+      response.Monday,
+      response.Tuesday,
+      response.Wednesday,
+      response.Thursday,
+      response.Friday,
+      response.Saturday
+    ]
+    interests = interestList.map(element => element.option_id)
 
-    res.status(201).json({token})
+    res.status(201).json({token, days, interests})
   } catch (e) {
     next({status: 400, error: `Volunteer could not be registered`})
   }
@@ -15,9 +26,20 @@ async function signup(req, res, next) {
 async function login(req, res, next) {
   try {
     const response = await model.login(req.body)
+    const interestList = await model.interestFinder(response.id)
     const token = auth.createToken(response.id)
+    const days = [
+      response.Sunday,
+      response.Monday,
+      response.Tuesday,
+      response.Wednesday,
+      response.Thursday,
+      response.Friday,
+      response.Saturday
+    ]
+    interests = interestList.map(element => element.option_id)
 
-    res.json({token})
+    res.json({token, days, interests})
   } catch (e) {
     next({status: 401, error: `Email or password is incorrect`})
   }
