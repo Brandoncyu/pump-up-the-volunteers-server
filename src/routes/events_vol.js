1b5db25574b355ed6ffdc9e26069d71d4d8c77cf
dtruong11 <dtruong11@users.noreply.github.com>
2018-08-08 10:00:32 -0700

    fixed bugs

--- src/routes/events_vol.js
+++ src/routes/events_vol.js
@@ -1,10 +1,9 @@
-const router = require('express').Router()
-const ctrl = require('../controllers/events_vol')
-const auth = require('../lib/auth')
+// const router = require('express').Router()
+// const ctrl = require('../controllers/events_vol')
+// const auth = require('../lib/auth')
 
-router.get('/', auth.isLoggedIn, ctrl.index)
-router.patch('/:id', auth.isAuthorized, ctrl.patch)
+// router.get('/', auth.isLoggedIn, ctrl.index)
+// router.patch('/:id', auth.isAuthorized, ctrl.patch)
 
-// router.getOne('/:id', auth.isLoggedIn, ctrl.getOne)
 
-module.exports = router
+// module.exports = router
