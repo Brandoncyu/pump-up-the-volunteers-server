37f06030ff689948296b4aa52e65b6c65e94200f
dtruong11 <dtruong11@users.noreply.github.com>
2018-08-08 17:32:49 -0700

    Volunteer table events

--- src/models/events_vol.js
+++ src/models/events_vol.js
@@ -1,20 +1,21 @@
 const db = require('../db/knex')
 
 function get(days, categories) {
+    const daysArr = JSON.parse(days)
+    const categoriesArr = JSON.parse(categories)
     return db('options')
         .join('organizations', 'organizations.option_id', "=", "options.id")
         .join('events', 'events.org_id', '=', 'organizations.id')
-        .whereIn('option_id', JSON.parse(categories))
-        //.whereIn('day', JSON.parse(days))
+        .whereIn('option_id', categoriesArr)
+        .whereIn('day', daysArr)
         .returning('*')
         .then(response => {
             console.log("I am the response", response)
             return response
         })
-        .then(console.log)
 }
+
 
 module.exports = {
     get
-
 }
